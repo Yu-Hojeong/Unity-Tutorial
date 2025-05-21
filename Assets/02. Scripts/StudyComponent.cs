90cd3036bd71e27312a053e3d14515e35e86bc95
Yu-Hojeong <yuhojeong0727@gmail.com>
2025-05-21 16:30:43 +0900

    Unity3
    스크립트를 컴포넌트에 추가
    오브젝트를 스크립트에 연결

---
+++ Assets/02. Scripts/StudyComponent.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class StudyComponent : MonoBehaviour
+{
+
+    public GameObject obj;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        obj.name = "cuuuuube";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
