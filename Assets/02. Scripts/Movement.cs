38a97667f40d33be5c14841b1f758f331a021ebd
Yu-Hojeong <yuhojeong0727@gmail.com>
2025-05-22 16:25:40 +0900

    Unity4
    컴포넌트 접근, 캐릭터 이동, 프리팹 생성과 소멸

---
+++ Assets/02. Scripts/Movement.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+ 
+ public class Movement : MonoBehaviour
+ {
+     public float moveSpeed = 5f;
+     
+     void Update()
+     {
+         /// Input System (Old - Legacy)
+         /// 입력값에 대한 약속된 시스템
+         /// 이동 -> WASD, 화살표키보드
+         /// 점프 -> Space
+         /// 총쏘기 -> 마우스 왼쪽
+         
+         float h = Input.GetAxis("Horizontal");
+         float v = Input.GetAxis("Vertical");
+         
+         Vector3 dir = new Vector3(h, 0, v);
+         Debug.Log($"현재 입력 : {dir}");
+         
+         transform.position += dir * moveSpeed * Time.deltaTime;
+     }
+ }
